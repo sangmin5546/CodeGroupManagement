049fe10f75ba455b6bfbf8904844a50db46eecea
lee1345 <swa1129@naver.com>
2024-11-25 15:12:37 +0900

    test

---
+++ src/main/java/fullstack/four/human/CodeGroupManagementApplication.java
@@ -0,0 +1,13 @@
+package fullstack.four.human;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGroupManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGroupManagementApplication.class, args);
+	}
+
+}
